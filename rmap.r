adac3129071fd8c49913245249b9d0d6736710a6
mpavlis <mclpvs@yahoo.co.uk>
2017-09-18 12:32:36 +0100

    split get_map to add_points and add_basemap_elements 2

--- rmap.r
+++ rmap.r
@@ -3,62 +3,56 @@ library(ggplot2)
 library(ggmap)
 library(ggsci)
 
-source("~/Dropbox/liverpool/papers/density/r_script/create_scale_bar.r")
+source("~/Dropbox/git/rMap/create_scale_bar.r")
 
-get_basemap <-  function(sfc_obj, ...){
+get_basemap <-  function(location, ...){
   
-  # this is the bounding box of the input dataset
-  bbox_sfc <- as.numeric(st_bbox(sfc_obj))
-  
-  # use get_map to get the basemap
-  basemap <- get_map(location = bbox_sfc, ...)
-  
-  # this is the bounding box of the basemap (xmin, ymin, xmax, ymax)
-  bbox_basemap <- as.numeric(attributes(basemap)$bb)[c(2,1,4,3)]
-  
-  # for as long as the bbox of the basemap is smaller than the bbox of the input, zoom out 
-  while (any(bbox_sfc[1:2] - bbox_basemap[1:2] < 0) | any(bbox_sfc[3:4] - bbox_basemap[3:4] > 0)){
-    zoom <- attributes(basemap)$zoom - 1
-    basemap <- get_map(location = bbox_sfc, zoom = zoom, ...)
+  if (is(location, "sf") | is(location, "sfc")){
+    
+    sfc_tr <- st_transform(st_geometry(location), 4326)
+    
+    # this is the bounding box of the input dataset
+    bbox_sfc <- as.numeric(st_bbox(sfc_tr))
+    
+    # use get_map to get the basemap
+    basemap <- get_map(location = bbox_sfc, ...)
+    
+    # this is the bounding box of the basemap (xmin, ymin, xmax, ymax)
     bbox_basemap <- as.numeric(attributes(basemap)$bb)[c(2,1,4,3)]
+    
+    # for as long as the bbox of the basemap is smaller than the bbox of the input, zoom out 
+    while (any(bbox_sfc[1:2] - bbox_basemap[1:2] < 0) | any(bbox_sfc[3:4] - bbox_basemap[3:4] > 0)){
+      zoom <- attributes(basemap)$zoom - 1
+      basemap <- get_map(location = bbox_sfc, zoom = zoom, ...)
+      bbox_basemap <- as.numeric(attributes(basemap)$bb)[c(2,1,4,3)]
+    }
+    return(basemap)
+  } else {
+    basemap <- get_map(location = location, ...)
+    return(basemap)
   }
-  return(basemap)
 }
 
-########################################################################################################################
-
-create_maps <- function(sf_df, zoom="auto", colour_by_fields = NULL, colour_object = NULL,
-                        scalebar_pos="bottomleft", orientation = T, alpha = 0.8, ...){
+get_basemap_elements <- function(basemap, orientation = T, arrow_north_size = 8, scalebar_pos="bottomleft"){
   
-  sfc_tr <- st_transform(sf_df$geometry, 4326)
-  coords <- do.call(rbind, unclass(sfc_tr))
-  sf_df$long <- coords[,1]
-  sf_df$lat <- coords[,2]
-  # bbox_sfc_tr <- st_bbox(sfc_tr)
-  # n <- ncol(sf_df)
-  
-  if (zoom == "auto"){
-    background <- get_basemap(sfc_obj = sfc_tr, ...)
-  }
-  
-  bbox_background <- attributes(background)$bb
+  bbox_basemap <- attributes(basemap)$bb
   # lon_dist <- distHaversine(c(bbox_background$ll.lon,bbox_background$ll.lat), c(bbox_background$ur.lon, bbox_background$ll.lat))
   # lat_dist <- distHaversine(c(bbox_background$ll.lon,bbox_background$ll.lat), c(bbox_background$ll.lon, bbox_background$ur.lat))
   
-  lat_dif <- bbox_background$ur.lat - bbox_background$ll.lat
-  lon_dif <- bbox_background$ur.lon - bbox_background$ll.lon
+  lon_dif <- bbox_basemap$ur.lon - bbox_basemap$ll.lon
+  lat_dif <- bbox_basemap$ur.lat - bbox_basemap$ll.lat
   if (scalebar_pos == "bottomleft"){
-    lat <- bbox_background$ll.lat + lat_dif/20
-    lon <- bbox_background$ll.lon + lon_dif/20
+    lon <- bbox_basemap$ll.lon + lon_dif/20
+    lat <- bbox_basemap$ll.lat + lat_dif/20
   } else if (scalebar_pos == "bottomright"){
-    lat <- bbox_background$ll.lat + lat_dif/20
-    lon <- bbox_background$ur.lon - lon_dif/20
+    lon <- bbox_basemap$ur.lon - lon_dif/3
+    lat <- bbox_basemap$ll.lat + lat_dif/10
   } else if (scalebar_pos == "topleft"){
-    lat <- bbox_background$ur.lat - lat_dif/5
-    lon <- bbox_background$ll.lon + lon_dif/10
+    lon <- bbox_basemap$ll.lon + lon_dif/20
+    lat <- bbox_basemap$ur.lat - lat_dif/5
   } else if (scalebar_pos == "topright"){
-    lat <- bbox_background$ur.lat - lat_dif/5
-    lon <- bbox_background$ur.lon - lon_dif/10
+    lon <- bbox_basemap$ur.lon - lon_dif/3
+    lat <- bbox_basemap$ur.lat - lat_dif/6
   }
   
   distance_lon <- lon_dif * 10
@@ -66,36 +60,61 @@ create_maps <- function(sf_df, zoom="auto", colour_by_fields = NULL, colour_obje
   distance_legend <- distance_lon/3
   arrow_length <- distance_lon/3
   arrow.distance <- distance_lon/2
-  # arrow_north_size <- ceiling(distance_lon * 10)
-  arrow_north_size <- 8
-  # if (arrow_north_size < 6) arrow_north_size <- 6
+  
+  if (orientation){
+    if (arrow_north_size == 0){
+      arrow_north_size <- ceiling(distance_lon * 10)
+    }
+  }
+  scaleBar(lon = lon, lat = lat, distanceLon = distance_lon, distanceLat = distance_lat, distanceLegend = distance_legend,
+           dist.unit = "km", arrow.length = arrow_length, arrow.distance = arrow.distance, arrow.North.size = arrow_north_size, orientation = orientation)
+}
+
+add_basemap_elements <- function(basemap, basemap_elements, extent = "device", ...){
+  ggmap(basemap, extent = extent, ...) +
+    basemap_elements
+}
+
+
+########################################################################################################################
+
+add_points <- function(basemap, sf_df, basemap_elements = NULL, colour_by_fields = NULL, colour = NULL,
+                       d3_palette = NULL, alpha_points = 0.8, legend_background = "grey", alpha_legend = 0.1,
+                       size = 2, legend_text = "", ...){
+  
+  sfc_tr <- st_transform(st_geometry(sf_df), 4326)
+  coords <- do.call(rbind, unclass(sfc_tr))
+  sf_df$long <- coords[,1]
+  sf_df$lat <- coords[,2]
   
   if (!is.null(colour_by_fields)){
     maps <- list()
     
     for (field_name in colour_by_fields){
       sf_df[[field_name]] <- as.factor(sf_df[[field_name]])
-      maps[[field_name]] <- ggmap(background, extent = 'device', legend = "bottomright") +
-        geom_point(data = sf_df, aes_string(x="long", y="lat", colour = field_name), size=2, alpha = alpha) +
-        # theme(panel.background = element_rect(fill = NA, colour = NA)) +
-        theme(legend.background = element_rect(fill=alpha('grey', 0.1))) + 
-        scaleBar(lon = lon, lat = lat, distanceLon = distance_lon, distanceLat = distance_lat, distanceLegend = distance_legend,
-                 dist.unit = "km", arrow.length = arrow_length, arrow.distance = arrow.distance, arrow.North.size = arrow_north_size, orientation = orientation) +
-        scale_color_d3(palette = "category20")
+      # map_temp <- ggmap(basemap, extent = 'device', legend = "bottomright") +
+      #   geom_point(data = sf_df, aes_string(x="long", y="lat", colour = field_name), size=size, alpha = alpha) +
+      #   # theme(panel.background = element_rect(fill = NA, colour = NA)) +
+      #   theme(legend.background = element_rect(fill=alpha('grey', 0.1))) + 
+      #   scaleBar(lon = lon, lat = lat, distanceLon = distance_lon, distanceLat = distance_lat, distanceLegend = distance_legend,
+      #            dist.unit = "km", arrow.length = arrow_length, arrow.distance = arrow.distance, arrow.North.size = arrow_north_size, orientation = orientation)
+      map_temp <- ggmap(basemap, extent = 'device', legend = "bottomright", padding = 0.05) +
+        geom_point(data = sf_df, aes_string(x="long", y="lat", colour = field_name), size=size, alpha = alpha_points) +
+        theme(legend.background = element_rect(fill=alpha(legend_background, alpha_legend))) +
+        basemap_elements
+                 
+      if (!is.null(d3_palette)){
+        map_temp <- map_temp + scale_color_d3(palette = d3_palette)
+      }
+      maps[[field_name]] <- map_temp
     }
-  } else if (!is.null(colour_object)){
-    maps <- list(ggmap(background, extent = 'device', legend = "bottomright", padding = 0.05) +
-                   geom_point(data = sf_df, aes(x=long, y=lat, colour = "location"), size=2, alpha = alpha, show.legend = T) +
-                   scale_colour_manual(name="Retail Units", values=c(location=colour_object)) + 
+  } else if (!is.null(colour)){
+    maps <- list(ggmap(basemap, extent = 'device', legend = "bottomright", padding = 0.05) +
+                   geom_point(data = sf_df, aes(x=long, y=lat, colour = "location"), size=size, alpha = alpha, show.legend = T) +
+                   scale_colour_manual(name=legend_text, values=c(location=colour)) + 
                    theme(legend.background = element_rect(fill=alpha('grey', 0.1)),
                          legend.text = element_text(size=rel(1.2)),
                          legend.title = element_text(size = rel(1.5))) + 
-                   scaleBar(lon = lon, lat = lat, distanceLon = distance_lon, distanceLat = distance_lat, distanceLegend = distance_legend,
-                            dist.unit = "km", arrow.length = arrow_length, arrow.distance = arrow.distance, arrow.North.size = arrow_north_size, orientation = orientation) #+
-                   # scale_color_d3(palette = "category20") 
-    )
-  } else {
-    maps <- list(ggmap(background, extent = 'device', legend = "bottomright", padding = 0.05) +
                    scaleBar(lon = lon, lat = lat, distanceLon = distance_lon, distanceLat = distance_lat, distanceLegend = distance_legend,
                             dist.unit = "km", arrow.length = arrow_length, arrow.distance = arrow.distance, arrow.North.size = arrow_north_size, orientation = orientation)
     )
